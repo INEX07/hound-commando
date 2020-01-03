1a897ca074d1210f87c3b2b7a92300f5d7dba12c
DeathHound6 <44831187+DeathHound6@users.noreply.github.com>
2020-01-03 10:32:53 +0000

    Create uptime.js

---
+++ commands/info/uptime.js
@@ -0,0 +1,67 @@
+const { Command, Client } = require("discord.js-commando");
+const client = new Client();
+const { MessageEmbed } = require("discord.js");
+
+module.exports = class uptime extends Command {
+  constructor(client) {
+    super(client, {
+      name: "uptime",
+      group: "info",
+      memberName: "uptime",
+      description: "Shows the bots uptime",
+      clientPermissions: ["EMBED_LINKS"],
+      guildOnly: true,
+      format: ""
+    });
+  }
+  run(message) {
+    let totalSeconds = this.client.uptime / 1000;
+    let days = Math.floor(totalSeconds / 86400);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    const embed = new MessageEmbed().setColor("RANDOM").setTitle("Uptime");
+    //uptimes
+    if (days > 1 && hours > 1 && minutes > 1) {
+      let uptime = `${days} days, ${hours} hours, and ${minutes} minutes`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 1 && hours > 1 && minutes > 1) {
+      let uptime = `${days} day, ${hours} hours, and ${minutes} minutes`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 1 && hours == 1 && minutes > 1) {
+      let uptime = `${days} day, ${hours} hour, and ${minutes} minutes`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 1 && hours == 1 && minutes == 1) {
+      let uptime = `${days} day, ${hours} hour, and ${minutes} minute`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 0 && hours > 1 && minutes > 1) {
+      let uptime = `${hours} hours, and ${minutes} minutes`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 0 && hours == 1 && minutes > 1) {
+      let uptime = `${hours} hour, and ${minutes} minutes`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 0 && hours == 1 && minutes == 1) {
+      let uptime = `${hours} hour, and ${minutes} minute`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 0 && hours == 0 && minutes > 1) {
+      let uptime = `${minutes} minutes`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 0 && hours == 0 && minutes == 1) {
+      let uptime = `${minutes} minute`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    } else if (days == 0 && hours == 0 && minutes == 0) {
+      let uptime = `Bot has just started up`;
+      embed.setDescription(uptime);
+      message.channel.send(embed);
+    }
+  }
+};
