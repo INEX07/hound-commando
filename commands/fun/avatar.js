c7fb3be8360985387a3b01b9580a070002717998
DeathHound6 <44831187+DeathHound6@users.noreply.github.com>
2020-01-03 10:26:09 +0000

    Create avatar.js

---
+++ commands/fun/avatar.js
@@ -0,0 +1,33 @@
+const {Command, Client} = require('discord.js-commando');
+const client = new Client();
+const Discord = require("discord.js");
+
+module.exports = class avatar extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'avatar',
+            group: 'fun',
+            memberName: 'avatar',
+            description: 'Sends a user\'s avatar, your own if no user specified',
+            clientPermissions: ["EMBED_LINKS"],
+            guildOnly: true,
+            format: "{USER_ID/USER_MENTION/blank}"
+        });
+    }
+    run(message) {
+      const args = message.content.slice(avatar.length).split(" ")
+      if(args[1]){
+      let user = this.client.users.get(args[1]) || message.mentions.users.first()
+        const embed = new Discord.MessageEmbed()
+            .setTitle(user.tag)
+            .setImage(user.displayAvatarURL())
+            .setColor("#0099ff");
+        message.channel.send(embed);
+    }else if(!args[1]){
+      let user = message.author
+      const embed = new Discord.MessageEmbed()
+      .setTitle(user.tag)
+      .setImage(user.displayAvatarURL)
+      .setColor("#0099ff");
+      message.channel.send(embed);
+    }}}
